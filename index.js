f2b73b03b44cca985873d198707db072e121ae01
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-21 14:08:51 -0500

    mongodb is sucessfully connected

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+const { MongoClient } = require('mongodb');
+
 
 // Set the port
 const PORT = process.env.PORT || 3000;
 
 
+// MongoDB connection URL
+const url = 'mongodb+srv://manojbishwakarma88:manoj123@recipesdatabase.iogvl.mongodb.net/';
+const dbName = 'RecipesDB'; 
 
+const client = new MongoClient(url);
+
+// Connect to the MongoDB server
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db(dbName);
+    } catch (err) {
+        console.error('Error connecting to MongoDB', err);
+    }
+}
+
+connectDB();
 
 // Start the server
 app.listen(PORT, () => {
