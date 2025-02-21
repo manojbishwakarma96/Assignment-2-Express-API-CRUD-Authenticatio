42061348ea89b8561291fe8d36e2bf83dbe47db4
LaxmanRky <laxman.rokaya02@gmail.com>
2025-02-21 15:17:49 -0500

    Added recipeRoutes with GET endpoint to fetch all recipes

--- Routes/recipeRoutes.js
+++ Routes/recipeRoutes.js
@@ -1,15 +1,16 @@
 /**
- * File Name: recipesRoutes.js
- * Student's Name: Manoj Bishwakarma, LAxman Rokaya
+ * File Name: recipeRoutes.js
+ * Student's Name: Manoj Bishwakarma, Laxman Rokaya
  * Student ID: 200594681, 200562874
- * Date: 2025-01-29
+ * Date: 2025-02-21
  */
+
 
 const express = require("express");
 const router = express.Router();
 const recipesController = require("../Controller/recipeController");
 
 // Fetch recipes from the database
-router.get("/getAllRecipes", recipesController.getRecipes);
+router.get("/", recipesController.getRecipes);
 
 module.exports = router;
