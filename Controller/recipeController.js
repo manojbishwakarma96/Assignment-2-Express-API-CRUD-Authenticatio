cfc742e9c22abd4c2881b7748faa9e161ad1e595
LaxmanRky <laxman.rokaya02@gmail.com>
2025-02-21 15:40:56 -0500

    Added Controller function for getRecipeById

--- Controller/recipeController.js
+++ Controller/recipeController.js
@@ -17,3 +17,14 @@ exports.getRecipes = async (req, res) => {
     }
 };
 
+exports.getRecipeById = async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({ success: false, message: "Recipe not found" });
+        }
+        res.status(200).json({ success: true, data: recipe });
+    } catch (error) {
+        res.status(500).json({ success: false, message: error.message });
+    }
+};
