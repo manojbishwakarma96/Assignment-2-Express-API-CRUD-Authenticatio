5ea728d06d59198bd63497c8bce27e8ac5bebacd
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-21 15:29:57 -0500

    Resolve database error by using Mongoose's find() method in getRecipes

--- Controller/recipeController.js
+++ Controller/recipeController.js
@@ -10,7 +10,7 @@ const Recipe = require("../models/Recipe");
 
 exports.getRecipes = async (req, res) => {
     try {
-        const recipes = await Recipe.find(); // Using mongoose's find method to get recipes
+        const recipes = await Recipe.find(); 
         res.status(200).json({ success: true, data: recipes });
     } catch (error) {
         res.status(500).json({ success: false, message: error.message });
--- Routes/recipeRoutes.js
+++ Routes/recipeRoutes.js
@@ -1,19 +1,15 @@
 /**
  * File Name: recipeRoutes.js
- * Student's Name: Manoj Bishwakarma, LAxman Rokaya
+ * Student's Name: Manoj Bishwakarma, Laxman Rokaya
  * Student ID: 200594681, 200562874
  * Date: 2025-02-21
- * Date: 2025-02-21
  */
-
-
 
 const express = require("express");
 const router = express.Router();
 const recipesController = require("../Controller/recipeController");
 
 // Fetch recipes from the database
-router.get("/", recipesController.getRecipes);
+router.get("/getAllRecipes", recipesController.getRecipes);
 
 module.exports = router;
-
