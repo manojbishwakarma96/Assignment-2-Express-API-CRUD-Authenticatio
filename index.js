046dddb73ceead631c14a30b24ab42ed470c02fb
manojbishwakarma <manojbishwakarma@neebal.com>
2025-02-21 13:53:15 -0500

    nodemon install for the realtime server

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ app.use(express.json());
 // Set the port
 const PORT = process.env.PORT || 3000;
 
+
+
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
